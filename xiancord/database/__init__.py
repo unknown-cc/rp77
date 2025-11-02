7777aed8982a300ce28a6b3934bab8f11deded83
unknown-cc <unknown.anonymous.cc@gmail.com>
2025-11-02 13:24:48 +0800

    slash_command

--- xiancord/database/__init__.py
+++ xiancord/database/__init__.py
@@ -5,12 +5,20 @@ import json
 import asyncio
 import os
 
-BASE_DIR = Path(__file__).resolve().parent
-credentials_path = BASE_DIR / "credentials.json"
 
 db = SpreadsheetManager()
 
 async def db_init():
+    BASE_DIR = "/etc/secrets/"
+    credentials_path = BASE_DIR / "credentials.json"
+    with open(credentials_path , "r", encoding="utf-8") as f:
+        creds = json.load(f)
+    await db.init(creds)
+    await db.add_spreadsheet("黑曜國際")
+
+async def db_init_local():
+    BASE_DIR = Path(__file__).resolve().parent
+    credentials_path = BASE_DIR / "credentials.json"
     with open(credentials_path , "r", encoding="utf-8") as f:
         creds = json.load(f)
     await db.init(creds)
